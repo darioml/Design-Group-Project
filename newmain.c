89f213e754838234bf96998fb9b1f05e524ac16f
Dario Magliocchetti <dario_dml@hotmail.com>
2012-03-01 17:33:55 +0000

    Work done on 1 March
    Fixed some of the older functions, removed comments
    Changed A/D handle
    Removed need of outputHigh

--- newmain.c
+++ newmain.c
@@ -3,10 +3,10 @@
 //#include <as16f917.h>
 #include <htc.h>
 #include <time.h>
+#include "newfile.c"
 //#include <>
 // Using Internal Clock of 8 Mhz
 #define FOSC 8000000L
-
 
 
 __CONFIG(FOSC_INTOSCCLK & WDTE_OFF);
@@ -16,8 +16,23 @@ __CONFIG(FOSC_INTOSCCLK & WDTE_OFF);
 // *************************************************
 time_t start, stop;
 int clock_t, ticks; long count;
-int turnflag = 0;
-int lastMotor = 0;
+int lastMotor;
+
+/*
+ function declerations
+ */
+void _delay_ms(unsigned int ms);
+void outputHighPin (bit pin);
+void outputLowPin (bit pin);
+void controlMotor(int Leftpt, int Rightpt);
+int searchTrack(void);
+int controlLCD();
+int controlBuzzer();
+void ADCInit(int ADC_Channel);
+unsigned int ADCRead(unsigned char ch);
+void InitialiseADC (unsigned char ADC_Channel);
+unsigned int ReadADC(unsigned char ADC_Channel);
+
 
 
 // *************************************************
@@ -26,15 +41,61 @@ int lastMotor = 0;
 #define _delay_us(x) { unsigned char us; \
 	  	       us = (x)/(12000000/FOSC)|1; \
 		       while(--us != 0) continue; }
+
 void _delay_ms(unsigned int ms)
 {
-  unsigned char i;
-  do {
-    i = 4;
-    do {
-      _delay_us(250);
-    } while(--i);
-  } while(--ms);
+    unsigned char i;
+    do
+    {
+        i = 4;
+        do
+        {
+            _delay_us(250);
+        } while(--i);
+    } while(--ms);
+}
+
+/* CHAR
+void outputHighPin (char pin)
+{
+    pin = 1;
+}
+void outputLowPin (char pin)
+{
+    pin = 0;
+}*/
+
+// *************************************************
+// Function to control the motor
+// Added function to help keep the bug on track
+// *************************************************
+
+void controlMotor(int Leftpt, int Rightpt)
+{
+	if (Leftpt == 1)
+        {		//if left sensor is on track, turn on right motor
+            //outputHighPin (RC1); // RC1 is right motor
+            RC1 = 1;
+	}
+        else
+        {		//if left sensor is off track, switch off right motor
+            //outputLowPin (RC1);
+            RC1 = 0;
+            lastMotor = 2;
+	}
+
+
+	if (Rightpt == 1)
+        {		//similarly, we do the same for the right motor
+            //outputHighPin (RC0); //RC0 is left motor
+            RA0 = 1;
+	}
+        else
+        {
+            //outputLowPin (RC0);
+            RA0 = 0;
+            lastMotor = 1;
+        }
 }
 
 
@@ -42,100 +103,22 @@ void _delay_ms(unsigned int ms)
 // Function to Search for the Track if the both sensors are off the track.
 // Used to cross the intersection in this way.
 // *************************************************
-int searchTrack(void){
-
-	//int lastMotor;		//0 if last motor switched off is leftMotor
-	int leftMotor;			//1 to switch on left motor
-	int rightMotor;			//1 to switch on right motor
-
+int searchTrack(void)
+{
 	if (lastMotor == 1)
-	{  //if the last motor switched off is the left motor, turn on left motor and off right motor
-		leftMotor = 1;
-		rightMotor = 0;
-	} else if (lastMotor == 2)
-
-	{ //if the last motor switched off is the right motor, turn on right motor and off left motor
-		leftMotor = 0;
-		rightMotor = 1;
+	{
+            controlMotor(1, 0);
 	}
-	turnflag = 0;
-	//controlMotor(leftMotor, rightMotor);		//call controlMotor to control the motor.
-
+        else if (lastMotor == 2)
+	{
+            controlMotor(0, 1);
+	}
 }
 
 
 
-// *************************************************
-// Function to control the motor
-// Added function to help keep the bug on track
-// *************************************************
-/*int controlMotor(Leftpt, Rightpt){
-
-	//turnflag = 0 - not turning
-	//turnflag = 1 - turning left
-	//turnflag = 2 - turning right
-
-	int timedifference, i;
-
-	if (Leftpt == 1){		//if left sensor is on track, turn on right motor
-		Output_high(//right motor pin); //the pins are like RA1, RB3, RC1
-
-		if (turnflag == 2){		//if the bug was previously turning right, bring the bug to move straight on the track again by switching off the opposite motor for half the time
-			turnflag = 0;
-			time(&end);
-			timedifference = difftime(stop, start);
-			Output_low (left motor);
-			lastMotor = 1;
-			i=0;
-
-			while(i< (timedifference / 2)){
-				i++;
-			}
-		Output_high (left motor);
-		}
-
-	}	else {		//if left sensor is off track, switch off right motor
-			Output_low (//right motor pin);
-			lastMotor = 2;
-
-			if (turnflag == 0){		//if the bug was not turning, record that it is now turning
-				turnflag = 2;
-				time(&start);
-			}
-	}
 
 
-	if (Rightpt == 1){		//similarly, we do the same for the right motor
-		Output_high(//left motor pin2);
-
-		if (turnflag == 1){
-			turnflag = 0;
-			time(&end);
-			timedifference = difftime(stop, start);
-			Output_low (right motor);
-			lastMotor = 2;
-			i=0;
-
-			while(i< (timedifference / 2)){
-				i++;
-			}
-		Output_high (right motor);
-		}
-
-	}	else {
-			Output_low(//left motor pin2);
-			lastMotor = 1;
-
-			if (turnflag == 0){
-				turnflag = 1;
-				time(&start);
-			}
-	}
-
-}
-
-
-*/
 // *************************************************
 // Function to control the LCD display
 // *************************************************
@@ -150,6 +133,7 @@ int controlLCD()
 // *************************************************
 // Function to control the buzzers
 // *************************************************
+
 int controlBuzzer()
 {
 
@@ -157,10 +141,59 @@ int controlBuzzer()
 }
 
 
+//Function to Initialise the ADC Module
+void ADCInit(int ADC_Channel)
+{
+   //We use default value for +/- Vref
 
+   //VCFG0=0,VCFG1=0
+   //That means +Vref = Vdd (5v) and -Vref=GEN
 
+   //Port Configuration
+   //We also use default value here too
+   //All ANx channels are Analog
 
+   /*
+      ADCON2
 
+      *ADC Result Right Justified.
+      *Acquisition Time = 2TAD
+      *Conversion Clock = 32 Tosc
+   */
+
+    if      (ADC_Channel == 0) 	TRISA0 = 1;
+	else if (ADC_Channel == 1) 	TRISA1 = 1;
+	else if (ADC_Channel == 2) 	TRISA2 = 1;
+	else if	(ADC_Channel == 3) 	TRISA3 = 1;
+
+        // Do we want to define this at the start of main???????????
+        //?!??!?!?!??!?!??!?!
+        // !?=?!?!?!?!?
+
+/* 	------
+	Specify all to be analogue inputs, refered to VDD.
+	Please refer to Page 118 from PIC manual for other configurations.
+ ------	*/
+
+	/* Analogue-RA0/RA1/RA3 Digital-RA2/RA5	*/
+	ADCON1	= 0b10000100;
+}
+
+//Function to Read given ADC channel (0-13)
+unsigned int ADCRead(unsigned char ch)
+{
+   ADCON0 = 0x00; //reset adcon, which is the main settings of the "A/D" conversion
+   ADCON0 = (ch<<2);   //Select ADC Channel
+
+   ADON=1;  //switch on the adc module
+
+   GO_DONE=1;//Start conversion
+   while(GO_DONE); //wait for the conversion to finish
+
+   ADON=0;  //switch off adc
+
+   return (ADRESH<8)|ADRESL;
+}
 
 
 /*************************************************************
@@ -191,16 +224,17 @@ unsigned int ReadADC(unsigned char ADC_Channel)
 {
     volatile unsigned int ADC_VALUE;
 
+    ADON = 1;
     /* Selecting ADC channel */
-    ADCON0 = (ADC_Channel << 3) + 1;				 /* Enable ADC, Fosc/2 */
+    ADCON0 = (ADC_Channel << 3) + 1;	/* Enable ADC, Fosc/2 */
 
-	ADIE	  =	0;									 	 		 	/* Masking the interrupt */
-    ADIF 	  = 0;								 /* Resetting the ADC interupt bit */
-	ADRESL	=	0; 						 /* Resetting the ADRES value register */
-	ADRESH	=	0;
+    ADIE     =	0;									 	 		 	/* Masking the interrupt */
+    ADIF     =      0;                  /* Resetting the ADC interupt bit */
+    ADRESL   =	0;              /* Resetting the ADRES value register */
+    ADRESH   =	0;
 
   //ADGO = 1;				  					  		 	/* Staring the ADC process */
-  ADCON0[2] = 1;
+  //ADCON0[2] = 1;
         while(!ADIF) continue;			   /* Wait for conversion complete */
 
 	ADC_VALUE	=	 ADRESL;										/* Getting HSB of CCP1 */
@@ -208,10 +242,6 @@ unsigned int ReadADC(unsigned char ADC_Channel)
 
   return (ADC_VALUE);     /* Return the value of the ADC process */
 }
-
-
-
-
 
 
 // *************************************************
@@ -230,44 +260,27 @@ int main(void)
 
     _delay_ms(8000);
 
-
     InitialiseADC (0);   // initialise channel   left transistor  /* Analogue-RA0/RA1/RA3 Digital-RA2/RA5	*/
     InitialiseADC (1);
-    int x,y, leftpt, rightpt;
+    int read, leftpt, rightpt;
 
     while (1) //let's continuously loop this, since it's controling our motor!
     {
-        /*test for left transistor*/
-        x = ReadADC(0);// get the input of analoge and return digital value of 10 bits, A2
+        /*test for left phototransistor*/
+        read = ReadADC(0);// get the input of analoge and return digital value of 10 bits, A2
+        leftpt = (read > 1.5) ? 1 : 0;
 
-        if (x > 1.5){
-            leftpt = 1;
-        }
-        else
-        {
-            leftpt = 0;
-        }
-
-        /*test for right transistor */
-
-        y=ReadADC(1);  // get the input of analoge and return digital value of 10 bits, A2D
-
-        if  (y > 1.5)
-        {
-            rightpt = 1;
-        }
-        else
-        {
-            rightpt = 0;
-        }
+        /*test for right phototransistor */
+        read = ReadADC(1);  // get the input of analoge and return digital value of 10 bits, A2D
+        rightpt = (read > 1.5) ? 1 : 0;
 
         if((leftpt==0) && (rightpt==0))
         {
-            //searchtrack();
+            searchTrack();
         }
         else
         {
-            //controlmotor(leftpt, rightpt);
+            controlMotor(leftpt, rightpt);
         }
     }
 }
